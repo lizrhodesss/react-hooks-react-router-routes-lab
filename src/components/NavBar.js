0f0a191c19f9b1dc174645d1a5cea16bc5e50800
liz <rhodes.liz@gmail.com>
2022-08-21 12:09:35 -0400

    commit tree 2ee321a9950abf601e5a2d7633e44a1bfa55f37b

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,22 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <NavLink 
+    to="/">
+    </NavLink>
+    <NavLink 
+      to="movies">
+    </NavLink>
+    <NavLink 
+      to="directors">
+    </NavLink>
+    <NavLink 
+      to="actors">
+    </NavLink>
+  </div>
+  )
 }
 
 export default NavBar;
